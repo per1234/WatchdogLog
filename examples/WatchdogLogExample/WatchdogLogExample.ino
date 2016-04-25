8fb54a5c0cb76f1923cce346bf36fcf23aec875d
per1234 <accounts@perglass.com>
2016-04-24 17:10:41 -0700

    Examples: comment improvements

--- examples/WatchdogLogExample/WatchdogLogExample.ino
+++ examples/WatchdogLogExample/WatchdogLogExample.ino
@@ -1,12 +1,12 @@
 // Demonstrates usage of the WatchdogLog library
 #include <WatchdogLog.h>
-#include <EEPROM.h>  //If you are using Arduino IDE version 1.0.x then you will need to install the EEPROM v2.0 library from here: https://github.com/arduino/Arduino/tree/ide-1.5.x/hardware/arduino/avr/libraries/EEPROM If you are using Arduino IDE version 1.6.2+ then the library is already installed.
+#include <EEPROM.h>  //If you are using an Arduino IDE version prior to 1.6.2 then you will need to install the EEPROM v2.0 library from here: https://github.com/arduino/Arduino/tree/ide-1.5.x/hardware/arduino/avr/libraries/EEPROM. If you are using Arduino IDE version 1.6.2 or greater then the library is already installed.
 
 const unsigned int EEPROMaddress = 0;  //EEPROM address to store the program address where the watchdog timeout reset occurred. The saved program address will take 4 bytes of EEPROM starting with this address.
 
 void setup() {
   Serial.begin(9600);
-  while (!Serial) {};  //for Leonardo et al. Wait for the serial monitor to be opened before proceeding with the sketch.
+  while (!Serial) {};  //for Leonardo et al. Wait for the Serial Monitor to be opened before proceeding with the sketch.
   Serial.println("\nProgram start");
   unsigned long programAddress;
   EEPROM.get(EEPROMaddress, programAddress);
@@ -20,7 +20,7 @@ void setup() {
     Serial.println("No program address logged");
   }
   Serial.println("Waiting for watchdog timeout reset...");
-  wdt_enable(WDTO_500MS);  //enable the watchdog timer with a 500MS second timeout duration
+  wdt_enable(WDTO_500MS);  //enable the watchdog timer with a 500 millisecond timeout duration
   WatchdogLog.begin(EEPROMaddress);  //initialize WatchdogLog
   while (true) {};  //perpetual loop until the watchdog times out
 }
